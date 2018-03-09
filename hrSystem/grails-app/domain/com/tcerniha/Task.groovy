7c0fc5348ea4c7ce0c6a509ecbc2aad8c1a1ca3d
TatjanaCerniha <b2041324@my.shu.ac.uk>
2018-03-09 16:12:12 +0000

    Created Domain Class Task

---
+++ hrSystem/grails-app/domain/com/tcerniha/Task.groovy
@@ -0,0 +1,21 @@
+package com.tcerniha
+
+class Task {
+	String taskName
+	int numberOfPeople
+	String sectionName
+	String department
+	String timeRequired
+	String description
+	Boolean taskCompleted
+
+    static constraints = {
+	taskName blank:false, nullable:false
+	numberOfPeople blank:false, nullable:false, min:2, max:10
+	sectionName blank:false, nullable:false
+	department blank:false, nullable:false
+	timeRequired blank:false, nullable:false
+	description blank:false, nullable:false, maxSize:1000, widget:'textarea'
+	taskCompleted blank:false, nullable:false
+    }
+}
