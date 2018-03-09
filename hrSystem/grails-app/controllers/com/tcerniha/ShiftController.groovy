10b220dd6fdd2774e12e40d80d854d28bcab476f
TatjanaCerniha <b2041324@my.shu.ac.uk>
2018-03-09 16:33:37 +0000

    Created Controller Class ShiftController and Generated views

---
+++ hrSystem/grails-app/controllers/com/tcerniha/ShiftController.groovy
@@ -0,0 +1,6 @@
+package com.tcerniha
+
+class ShiftController {
+
+    def scaffold = Shift
+}
--- hrSystem/grails-app/domain/com/tcerniha/Shift.groovy
+++ hrSystem/grails-app/domain/com/tcerniha/Shift.groovy
@@ -7,7 +7,7 @@ class Shift {
 	String startingTime
 
     static constraints = {
-	enum timeOfDay {Morning, Afternoon,Evening} blank:false, nullable:false
+	timeOfDay  blank:false, nullable:false
 	dayOfWeek blank:false, nullable:false
 	numberOfHours blank:false, nullable:false, min:4, max:10
 	startingTime blank:false, nullable:false
